29a95fe190c31d2efbebbb97b0b687717800d608
Evgenii <zkhasanovv@gmail.com>
2020-06-05 17:12:18 +0300

    Init

---
+++ board/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=150)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='task')
+    created_at = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
+    is_priority = models.BooleanField(default=False)
