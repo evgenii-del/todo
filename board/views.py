c0017e8c52977e5ba95c71cd4281814721761476
Evgenii <zkhasanovv@gmail.com>
2020-06-06 11:32:22 +0300

    Change permissions for actions

--- board/views.py
+++ board/views.py
@@ -30,25 +30,27 @@ class TaskViewSet(viewsets.GenericViewSet,
         return queryset
 
     @action(detail=True, methods=["patch"])
-    def done(self, *args, **kwargs):
-        task = Task.objects.filter(pk=kwargs["pk"])
+    def done(self, request, *args, **kwargs):
+        task = Task.objects.filter(pk=self.kwargs["pk"])
 
-        if task[0].is_done:
-            task.update(is_done=False)
-        else:
-            task.update(is_done=True)
-            task.update(is_priority=False)
-
-        return Response(status=201)
+        if task[0].owner == self.request.user:
+            if task[0].is_done:
+                task.update(is_done=False)
+            else:
+                task.update(is_done=True)
+                task.update(is_priority=False)
+            return Response(status=201)
+        return Response(status=403)
 
     @action(detail=True, methods=["patch"])
-    def priority(self, *args, **kwargs):
-        task = Task.objects.filter(pk=kwargs["pk"])
+    def priority(self, request, *args, **kwargs):
+        task = Task.objects.filter(pk=self.kwargs["pk"])
 
-        if task[0].is_priority:
-            task.update(is_priority=False)
-        else:
-            task.update(is_priority=True)
-            task.update(is_done=False)
-
-        return Response(status=201)
+        if task[0].owner == self.request.user:
+            if task[0].is_priority:
+                task.update(is_priority=False)
+            else:
+                task.update(is_priority=True)
+                task.update(is_done=False)
+            return Response(status=201)
+        return Response(status=403)
